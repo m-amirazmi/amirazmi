27fd41b9fb486fcb5bd820f2d4414d8475117f3d
Muhamad Amir <m.amirazmi@hotmail.com>
2023-07-01 11:18:05 +0800

    refactor the menu links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,8 @@
 import { Props } from "@/utils/types";
 import { Roboto } from "next/font/google";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import menuData from "@/data/menu.json";
 
 const font = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -8,36 +10,44 @@ const font = Roboto({
 });
 
 export default function Layout({ children }: Props) {
+  const { pathname } = useRouter();
+
   return (
     <main
-      className={`${font.className} max-w-4xl mx-auto py-10 md:px-0 px-6 flex w-full gap-6`}
+      className={`${font.className} max-w-3xl mx-auto py-6 md:py-10 px-6 flex w-full gap-6`}
     >
-      <aside className="w-2/12 ">
-        <div className="sticky top-10 left-0 flex items-end flex-col text-lg">
-          <Link className="border-r-2 border-slate-700 pr-4 py-2" href="/">
-            Home
-          </Link>
-          <Link className="border-r-2 border-slate-700 pr-4 py-2" href="/about">
-            About
-          </Link>
-          <Link
-            className="border-r-2 border-slate-700 pr-4 py-2"
-            href="/projects"
-          >
-            Projects
-          </Link>
-          <Link className="border-r-2 border-slate-700 pr-4 py-2" href="/blogs">
-            Blogs
-          </Link>
-        </div>
-      </aside>
-      <div className="w-10/12 flex flex-col gap-6">
-        <header className="bg-gradient-to-br from-slate-800 to-slate-800/90 text-slate-50 flex items-center justify-center flex-col py-10 gap-4 rounded-xl">
+      <div className="w-full flex flex-col">
+        <header className="bg-gradient-to-br from-slate-800 to-slate-800/90 text-slate-50 flex items-center justify-center flex-col py-10 gap-3 rounded-xl">
           <h1 className="text-3xl uppercase tracking-widest font-bold">
             Amir Azmi
           </h1>
           <p>Front End Developer</p>
         </header>
+
+        <div className="gap-4 flex items-end py-4 max-w-xl w-full mx-auto text-sm md:text-base">
+          {menuData.map((i) => {
+            let activeClass =
+              "hover:bg-slate-200/90 text-slate-800/50 hover:text-slate-800";
+
+            if (
+              (i.pathname === "/" && pathname === "/") ||
+              (i.pathname !== "/" && pathname.includes(i.pathname))
+            ) {
+              activeClass = "bg-slate-800 text-white";
+            }
+
+            return (
+              <Link
+                key={i.pathname}
+                className={`block w-full text-center bg-slate-200/50 rounded-lg py-2 transition-all duration-150 ease-linear ${activeClass}`}
+                href={i.pathname}
+              >
+                {i.title}
+              </Link>
+            );
+          })}
+        </div>
+
         <div>{children}</div>
       </div>
     </main>
