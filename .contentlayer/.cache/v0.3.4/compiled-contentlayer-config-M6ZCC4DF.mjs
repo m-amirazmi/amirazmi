270628bcbf7a700e316a0d25b11ddcb70bde139e
Muhamad Amir <m.amirazmi@hotmail.com>
2023-07-01 13:02:45 +0800

    add contentlayer

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-M6ZCC4DF.mjs
@@ -0,0 +1,28 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    subtitle: { type: "string" },
+    date: { type: "date", required: true },
+    thumbnail: { type: "string" }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "src/data/posts",
+  documentTypes: [Post]
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-M6ZCC4DF.mjs.map
