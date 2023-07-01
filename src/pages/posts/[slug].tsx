666ec4a059c28153ea1166e6276a514f8c79f82c
Muhamad Amir <m.amirazmi@hotmail.com>
2023-07-01 12:56:06 +0800

    update the post listing and add some modification to post

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,46 @@
+import { allPosts, Post } from "contentlayer/generated";
+import { GetStaticProps } from "next";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PostPage({ post }: { post: Post }) {
+  const MDXContent = useMDXComponent(post.body.code);
+
+  return (
+    <>
+      <Link
+        href="/posts"
+        className="underline hover:text-slate-500 transition-colors duration-150 ease-linear my-4 block"
+      >
+        Back to Posts
+      </Link>
+      <div className="w-[720px] h-[320px] mb-8">
+        <Image
+          src={post.thumbnail as string}
+          alt={post.title}
+          width={720}
+          height={300}
+          className="w-full h-full object-center object-cover rounded-xl"
+        />
+      </div>
+      <div className="prose md:prose-xl">
+        <h1>{post.title}</h1>
+        <MDXContent />
+      </div>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  const paths = allPosts.map((post) => `/posts/${post._raw.flattenedPath}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  const slug = params!.slug;
+  const post = allPosts.find((p) => p._raw.flattenedPath === slug);
+
+  if (!post) return { notFound: true };
+  return { props: { post } };
+};
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,50 @@
+import { Post, allPosts } from "contentlayer/generated";
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import Image from "next/image";
+
+export default function PostsListPage({ posts }: { posts: Post[] }) {
+  return (
+    <div className="flex flex-wrap -mx-2 items-stretch">
+      {posts.map((p) => {
+        const createdAt = formatRelative(
+          subDays(new Date(p.date), 3),
+          new Date()
+        );
+        return (
+          <div className="w-1/2 p-2" key={p._id}>
+            <Link
+              className="block border rounded-xl h-full hover:border-slate-800/50 transition-colors duration-150 ease-linear"
+              href={p.url}
+            >
+              {p.thumbnail && (
+                <div className="aspect-[720/320]">
+                  <Image
+                    src={p.thumbnail as string}
+                    alt={p.title}
+                    width={720}
+                    height={300}
+                    className="rounded-t-xl w-full h-full object-cover object-center"
+                  />
+                </div>
+              )}
+              <div className="p-4">
+                <h2 className="text-xl mb-1">{p.title}</h2>
+                <p className="text-sm text-slate-500 mb-2">{createdAt}</p>
+                <p className="text-slate-600">{p.subtitle}</p>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  const posts = allPosts;
+
+  if (!posts) return { notFound: true };
+  return { props: { posts } };
+};
